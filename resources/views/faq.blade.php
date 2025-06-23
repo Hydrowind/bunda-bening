d683421aa6b046cd9db778d2d50bc5fb6efb92ae
u072654 <herdhiantoko_sandra@bca.co.id>
2025-06-23 14:53:18 +0700

    Test

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -9,7 +9,7 @@
     <div class="container text-center">
       <div class="d-flex flex-column justify-content-center align-items-center">
         <h1 data-aos="fade-up">FAQ <span>Bunda Bening</span></h1>
-        {{-- <p data-aos="fade-up" data-aos-delay="100">Berbagai berita dan informasi terbaru seputar Bunda Bening<br></p> --}}
+        {{-- <p data-aos="fade-up" data-aos-delay="100">Berbagai berita dan informasi terbaru seputar Bunda Bening <br></p> --}}
         {{-- <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
           <a href="#about" class="btn-get-started">Get Started</a>
           <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
